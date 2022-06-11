a6ef63271bac14fbd594757bfa329ee71ea5bd38
princegobah <babynamek@gmail.com>
2022-06-10 20:24:54 -0400

    commit tree 7191aba682a910b2a7f1bc85245af8ed7f034fd0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_accessor :knowledge
+   def initialize 
+    @knowledge=[]
+   end
+   def learn(arg)
+    @knowledge<<arg
+   end
 end
